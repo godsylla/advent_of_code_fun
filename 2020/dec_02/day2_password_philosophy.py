15368998f5bd46b336471b6e6353a2e3b4e28dfe
godsylla <sylvi.tran@gmail.com>
2020-12-05 15:18:20 -0800

    day2 done

--- 2020/dec_02/day2_password_philosophy.py
+++ 2020/dec_02/day2_password_philosophy.py
@@ -1,17 +1,38 @@
 from collections import Counter
+import numpy as np 
 
-def count_valid_pws (pws:list, policies:list) -> int:
-    assert len(pws) == len(policies)
+def parse_input (raw_input_lines:list) -> tuple:
+    policies = [policy_pw.split(': ')[0] for policy_pw in raw_input_lines]
+    chars = [policy[-1] for policy in policies]
+    lower = [int(policy.split(' ')[0].split('-')[0]) for policy in policies]
+    upper = [int(policy.split(' ')[0].split('-')[1]) for policy in policies]
+    
+    passwords = [policy_pw.split(': ')[1].rstrip('\n') for policy_pw in raw_input_lines]
 
-    char_min = [int(policy.split(' ')[0].split('-')[0]) for policy in policies]
-    char_max = [int(policy.split(' ')[0].split('-')[1]) for policy in policies]
-    char = [policy[-1] for policy in p1_policies]
+    return lower, upper, chars, passwords
+
+
+def count_valid_pws (raw_input_lines:list) -> int:
+    mins, maxs, chars, pws = parse_input (raw_input_lines)
 
     counter = 0
     for i, pw in enumerate(pws):
         counts = Counter(pw)
-        if counts.get(char[i]) and counts.get(char[i]) <= char_max[i] and counts.get(char[i]) >= char_min[i]:
+        if counts.get(chars[i]) and counts.get(chars[i]) <= maxs[i] and counts.get(chars[i]) >= mins[i]:
             counter += 1
+    
+    return counter
+
+
+def valid_char_locs (raw_input_lines:list) -> int:
+    index_1, index_2, chars, pws = parse_input (raw_input_lines)
+
+    counter = 0
+    for i, pw in enumerate(pws):
+        if chars[i] in pw:
+            indices = [ind + 1 for ind, ltr in enumerate(pw) if ltr == chars[i]]
+            if (index_1[i] in indices and index_2[i] not in indices) or (index_1[i] not in indices and index_2[i] in indices):
+                counter += 1
     
     return counter
 
@@ -20,16 +41,16 @@ if __name__ == '__main__':
     # Read in the data
     with open('./day2_part1_input.txt', 'r') as f:
         part1_raw = f.readlines()
-
-    # Format the data
-    p1_policies = [policy_pw.split(':')[0] for policy_pw in part1_raw]
-    p1_pws = [policy_pw.split(':')[1].lstrip(' ').rstrip('\n') for policy_pw in part1_raw]
-
+    
+    with open('./day2_part2_input.txt', 'r') as f2:
+        part2_raw = f2.readlines()
+    
     # Part 1
-    valid_pws = count_valid_pws(p1_pws, p1_policies)
-    print(f'Valid Passwords: {valid_pws}')
+    valid_pws1 = count_valid_pws(part1_raw)
+    print(f'Valid Passwords: {valid_pws1}')
     print()
 
     # Part 2
-    
+    valid_pws2 = valid_char_locs(part2_raw)
+    print(f'Revised Valid Passwords: {valid_pws2}')
     
