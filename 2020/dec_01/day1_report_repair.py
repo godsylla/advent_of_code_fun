5a3960c8cd9194a5b23d3086931ecaf7b57f5e58
godsylla <sylvi.tran@gmail.com>
2020-12-04 20:03:13 -0800

    day1 complete

--- 2020/dec_01/day1_report_repair.py
+++ 2020/dec_01/day1_report_repair.py
@@ -1,6 +1,6 @@
 
 
-def find_pair_and_multiply(numbers: list, total: int=2020) -> float:
+def find_pair (numbers: list, total: int=2020) -> float:
 
     pair = tuple()
     for num in numbers:
@@ -11,38 +11,47 @@ def find_pair_and_multiply(numbers: list, total: int=2020) -> float:
     return pair
 
 
-def find_triplet_and_multiply(numbers: list, total:int=2020) -> float:
+def find_triplet (numbers: list, total:int=2020) -> float:
 
     triplet = tuple()
 
     for num in numbers:
         diff_to_split = total - num
+
         try:
             subset = numbers.copy()
             subset.remove(num)
-            if find_pair_and_multiply(subset, diff_to_split):find_pair_and_multiply(subset, diff_to_split)
-                triplet = (num, )
-
-
+            
+            pair = find_pair (subset, diff_to_split)
+            
+            if len(pair) != 0:
+                triplet = (num, pair[0], pair[1])
+            
         except:
             pass
 
-
-
+    return triplet
 
 
 if __name__ == '__main__':
     # Read in the input
-    with open('./day1_input.txt', 'r') as f:
-        txt_numbers = f.readlines()
+    with open('./day1_part1_input.txt', 'r') as f:
+        pt1_txt_numbers = f.readlines()
+    with open('./day1_part2_input.txt', 'r') as f:
+        pt2_txt_numbers = f.readlines()
 
     # Format the raw input
-    numbers = [int(num.strip('\n')) for num in txt_numbers]
+    numbers_1 = [int(num.strip('\n')) for num in pt1_txt_numbers]
+    numbers_2 = [int(num.strip('\n')) for num in pt2_txt_numbers]
+    print(len(numbers_2))
 
-    p1_pair = find_pair_and_multiply(numbers)
+    p1_pair = find_pair (numbers_1)
+    print(p1_pair)
     print(f'Part 1 answer: {p1_pair[0] * p1_pair[1]}')
 
-    p2_triplet =
+    p2_triplet = find_triplet(numbers_2)
+    print(p2_triplet)
+    print(f"Part 2 answer: {p2_triplet[0] * p2_triplet[1] * p2_triplet[2]}")
 
 
 
